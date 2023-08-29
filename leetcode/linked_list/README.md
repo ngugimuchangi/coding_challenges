332c8c489653f9edcc9cc0756a0350d4e9fed980
ngugimuchangi <tech.curious14@gmail.com>
2023-08-29 05:46:59 +0300

    check if linked list is a palindrome

---
+++ leetcode/linked_list/README.md
@@ -0,0 +1,3 @@
+# Linked List Problems
+
+1. [Palindrome Linked List](palindrome_linked_list.py)
---
+++ leetcode/linked_list/palindrome_linked_list.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""
+Leetcode 234. Palindrome Linked List
+https://leetcode.com/problems/palindrome-linked-list/
+
+"""
+from typing import Optional
+
+
+class ListNode:
+    """
+    Linked list node
+    """
+
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        """
+        Check if linked list is palindrome
+        Time complexity: O(n)
+        Space complexity: O(1)
+        Approach: Reverse half of linked list
+         - Use Floyd's tortoise and hare algorithm to get mid points
+         - Reverse the second half of the linked list
+         - Compare the first half and the second half
+
+        """
+        slow, fast, prev = head, head, None
+
+        # Get mid points
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+
+        # Reverse half of linked list and adjust pointers
+        while (slow):
+            slow.next, prev, slow = prev, slow, slow.next
+
+        # Check if list is palindrome with two pointers
+        slow, fast = head, prev
+        while fast:
+            if slow.val != fast.val:
+                return False
+            slow, fast = slow.next, fast.next
+        return True
