f16ba40430cc02a0aee0a88f07c6599c63afe0b2
ngugimuchangi <tech.curious14@gmail.com>
2023-08-31 10:04:25 +0300

    conactentate unique strings

---
+++ leetcode/backtracking/maximum_length_of_a_concatenated_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Leetcode 1239. Maximum Length of a Concatenated String with Unique Characters
+https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+"""
+
+from typing import List
+
+
+def maxLength(arr: List[str]) -> int:
+    NotImplemented
