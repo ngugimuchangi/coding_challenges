88bcbe15fa33619fbdc4a8cd1944bf0047ef71e4
ngugimuchangi <tech.curious14@gmail.com>
2023-09-09 17:18:45 +0300

    update solution docs

--- leetcode/array_and_hashing/summary_ranges.py
+++ leetcode/array_and_hashing/summary_ranges.py
@@ -6,6 +6,14 @@ from typing import List
 
 
 def summaryRanges(nums: List[int]) -> List[str]:
+    """
+    Time complexity: O(n)
+    Space complexity: O(n)
+    Approach:
+        1. Use two pointers to keep track of the start and end of the range
+        2. If the next number is not the next number in the range, append the range to the result
+        3. Update the start pointer to the next number
+    """
     lookup, res = set(nums), []
     l = 0
     for r in range(len(nums)):
