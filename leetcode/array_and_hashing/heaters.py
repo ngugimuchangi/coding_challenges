a6c50ee37dec79e41259c14b9d24a93a7a30c543
ngugimuchangi <tech.curious14@gmail.com>
2023-09-09 17:05:48 +0300

    finding minimum radius of heaters

---
+++ leetcode/array_and_hashing/heaters.py
@@ -0,0 +1,30 @@
+"""
+Leetcode 475: Heaters
+https://leetcode.com/problems/heaters/
+"""
+
+
+from typing import List
+
+
+def findRadius(houses: List[int], heaters: List[int]) -> int:
+    """
+    Time complexity: O(nlogn) - sorting
+    Space complexity: O(1)
+    Approach:
+        1. Sort the houses and heaters
+        2. For each house, find the closest heater on the left and right
+        3. Return the maximum distance
+    """
+    radius = l = r = pos = 0
+    len_heaters = len(heaters)
+    houses.sort()
+    heaters.sort()
+    for house in houses:
+        while pos < len_heaters - 1 and house >= heaters[pos]:
+            pos += 1
+        l = abs(house - (heaters[pos - 1] if pos > 0 else heaters[pos]))
+        r = abs(house - heaters[pos])
+        radius = max(radius, min(l, r))
+
+    return radius
