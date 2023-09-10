7aea68ff9098dcb513a9ed74864ec62603a06cc8
ngugimuchangi <tech.curious14@gmail.com>
2023-09-10 17:30:17 +0300

    new solution for frog jump problem

--- leetcode/dynamic_programming/frog_jump.py
+++ leetcode/dynamic_programming/frog_jump.py
@@ -8,8 +8,8 @@ from typing import List
 def canCross(stones: List[int]) -> bool:
     """
     - Dynamic programming problem
-    - Time complexity: O(n^2) - n is the length of the stones
-    - Space complexity: O(n^2) - n is the length of the stones
+    - Time complexity: O(n^3) - n is the maximum number of stones
+    - Space complexity: O(n)
     - Approach: Dynamic programming
         - Use a dictionary to store the possible jumps from each stone
         - The key is the stone and the value is a set of possible jumps
@@ -21,10 +21,6 @@ def canCross(stones: List[int]) -> bool:
 
     def canCrossNext(prev, k):
         stone = prev + k
-        # k == 0 is not allowed because the frog must jump forward
-        # stone not in stones_set is not allowed because the frog must jump to a stone
-        # (stone, k) in visited is not allowed because the frog must not visit the same stone with the same jump size
-        # reduces the time complexity from O(n^3) to O(n^2)
         if not k or stone not in stones_set or (stone, k) in visited:
             return False
         visited.add((stone, k))
@@ -33,3 +29,34 @@ def canCross(stones: List[int]) -> bool:
         return canCrossNext(stone, k - 1) or canCrossNext(stone, k) or canCrossNext(stone, k + 1)
 
     return canCrossNext(stones[0], 1)
+
+
+def canCrossNextTwo(stones: List[int]) -> bool:
+    """
+    - Dynamic programming problem
+    - Time complexity: O(n^3) - n is the maximum number of stones
+    - Space complexity: O(n)
+    - Approach: Dynamic programming - Memoization
+        - Use a dictionary to store the possible jumps from each stone
+        - The key is the stone and the value is a set of possible jumps
+        - The possible jumps from a stone is the union of the possible jumps
+            from the previous stones that can reach the current stone
+    """
+    stones_set = set(stones)
+
+    def canCrossNext(stone: int, k: int, memo: dict = {}):
+        if (stone, k) in memo:
+            return memo[(stone, k)]
+        if not k or stone not in stones_set:
+            return False
+        if stone == stones[-1]:
+            return True
+        for i in range(k - 1, k + 2):
+            can_cross = canCrossNext(stone + i, i, memo)
+            if can_cross:
+                memo[(stone, k)] = True
+                return True
+        memo[(stone, k)] = False
+        return False
+
+    return False if stones[1] - stones[0] > 1 else canCrossNext(stones[1], 1)
