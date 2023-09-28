db5a31718640f92b022bee592c72e053cb52e42c
ngugimuchangi <tech.curious14@gmail.com>
2023-09-28 09:31:38 +0300

    keep track of res using start and end of variable instead of copying it

--- leetcode/dynamic_programming/longest_palindromic_substring.py
+++ leetcode/dynamic_programming/longest_palindromic_substring.py
@@ -12,20 +12,20 @@ Analysis: Time O(n^2) | Space O(n) - n is the length of the string
 
 
 def longestPalindrome(s: str) -> str:
-    res = ''
-    len_s, res_len = len(s), 0
+    start, end = 0, 0
+    len_s = len(s)
 
-    def check_and_update_res(mid, odd=True):
-        nonlocal res, res_len
-        l, r = mid, mid if odd else mid + 1
+    def check_and_update_res(left, right):
+        l, r = left, right
         while l >= 0 and r < len_s and s[l] == s[r]:
             l, r = l - 1, r + 1
-        if r - l + 1 > res_len:
-            res = s[l + 1: r]
-            res_len = r - l + 1
+        l += 1
+        if r - l > end - start:
+            return l, r
+        return start, end
 
     for i in range(len_s):
-        check_and_update_res(i, odd=True)
-        check_and_update_res(i, odd=False)
+        start, end = check_and_update_res(i, i)
+        start, end = check_and_update_res(i, i + 1)
 
-    return res
+    return s[start: end]
